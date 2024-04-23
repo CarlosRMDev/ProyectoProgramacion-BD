f9ad1ed82bc1c33da297a9f60900c1b259df046d
inknot8xxx <amb0055@alu.medac.es>
2024-04-23 11:03:33 +0200

    Se ha finalizado el inicio de sesion, el registro y se ha comenzado el menú de juego

--- src/InterfazInicio/Registro.java
+++ src/InterfazInicio/Registro.java
@@ -1,21 +1,22 @@
 package InterfazInicio;
 
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
-import BaseDatos.ConexionMySQL;
-
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
 import java.awt.event.ActionListener;
-import java.sql.SQLException;
 import java.awt.event.ActionEvent;
-import java.util.Calendar;
-import java.util.Date;
+import java.awt.Color;
+import java.awt.Dimension;
 
 public class Registro extends JFrame {
 
@@ -27,11 +28,8 @@ public class Registro extends JFrame {
     private JPasswordField passwordField;
     private JTextField textFieldCorreo;
     private JTextField textFieldFechaNacimiento;
-    
-    // Creación de la ventana de registro
 
     public static void main(String[] args) {
-    	ConexionMySQL conexion = new ConexionMySQL("root", "test", "ProyectoBD");
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
@@ -44,90 +42,128 @@ public class Registro extends JFrame {
         });
     }
 
-    //Vamos a crear cada uno de los bloques necesarios para realizar el registro.
-    //Tenemos que unir todos los bloques de registro con nuestra base de datos.
     public Registro() {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Esto es para que la X sea para cerrar el programa
-        setExtendedState(JFrame.MAXIMIZED_BOTH); //Para que aparezca en pantalla completa
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         setBounds(100, 100, 526, 375);
+        
+        // Configuración del panel principal con GridBagLayout
         contentPane = new JPanel();
         contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        contentPane.setBackground(Color.DARK_GRAY);
         setContentPane(contentPane);
-        contentPane.setLayout(null);
+        contentPane.setLayout(new GridBagLayout());
 
-        //Texto de Usuario
-        textFieldUsuario = new JTextField();
-        textFieldUsuario.setBounds(188, 68, 130, 26);
-        contentPane.add(textFieldUsuario);
-        textFieldUsuario.setColumns(10);
+        // Configuración de los componentes de registro
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.fill = GridBagConstraints.HORIZONTAL;
 
-        //Ventana de Usuario
         JLabel lblUsuario = new JLabel("Nombre de usuario");
-        lblUsuario.setBounds(80, 73, 120, 16);
-        contentPane.add(lblUsuario);
+        lblUsuario.setForeground(Color.GREEN);
+        lblUsuario.setFont(new Font("Miriam Libre", Font.BOLD, 14));
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        contentPane.add(lblUsuario, gbc);
 
-        //Texto de Apellido1
-        textFieldApellido1 = new JTextField();
-        textFieldApellido1.setBounds(188, 106, 130, 26);
-        contentPane.add(textFieldApellido1);
-        textFieldApellido1.setColumns(20);
+        textFieldUsuario = new JTextField();
+        textFieldUsuario.setForeground(Color.BLACK);
+        textFieldUsuario.setColumns(20); // Aumentamos el número de columnas para que el campo sea más largo
+        textFieldUsuario.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        contentPane.add(textFieldUsuario, gbc);
 
-        //Ventana de Apellido1
         JLabel lblApellido1 = new JLabel("Apellido1");
-        lblApellido1.setBounds(130, 111, 76, 16);
-        contentPane.add(lblApellido1);
+        lblApellido1.setForeground(Color.GREEN);
+        lblApellido1.setFont(new Font("Miriam Libre", Font.BOLD, 14));
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        contentPane.add(lblApellido1, gbc);
 
-        //Texto de Apellido2
-        textFieldApellido2 = new JTextField();
-        textFieldApellido2.setBounds(188, 144, 130, 26);
-        contentPane.add(textFieldApellido2);
-        textFieldApellido2.setColumns(10);
+        textFieldApellido1 = new JTextField();
+        textFieldApellido1.setForeground(Color.BLACK);
+        textFieldApellido1.setColumns(20); // Aumentamos el número de columnas para que el campo sea más largo
+        textFieldApellido1.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        contentPane.add(textFieldApellido1, gbc);
 
-        //Ventana de Apellido2
         JLabel lblApellido2 = new JLabel("Apellido2");
-        lblApellido2.setBounds(130, 149, 76, 16);
-        contentPane.add(lblApellido2);
+        lblApellido2.setForeground(Color.GREEN);
+        lblApellido2.setFont(new Font("Miriam Libre", Font.BOLD, 14));
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        contentPane.add(lblApellido2, gbc);
 
-        //Texto de Password
-        passwordField = new JPasswordField();
-        passwordField.setBounds(188, 182, 130, 26);
-        contentPane.add(passwordField);
+        textFieldApellido2 = new JTextField();
+        textFieldApellido2.setForeground(Color.BLACK);
+        textFieldApellido2.setColumns(20); // Aumentamos el número de columnas para que el campo sea más largo
+        textFieldApellido2.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        contentPane.add(textFieldApellido2, gbc);
 
-        //Ventana de Password
         JLabel lblContraseña = new JLabel("Contraseña");
-        lblContraseña.setBounds(110, 187, 76, 16);
-        contentPane.add(lblContraseña);
+        lblContraseña.setForeground(Color.GREEN);
+        lblContraseña.setFont(new Font("Miriam Libre", Font.BOLD, 14));
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        contentPane.add(lblContraseña, gbc);
 
-        //Texto de Correo
-        textFieldCorreo = new JTextField();
-        textFieldCorreo.setBounds(188, 220, 130, 26);
-        contentPane.add(textFieldCorreo);
-        textFieldCorreo.setColumns(10);
+        passwordField = new JPasswordField();
+        gbc.gridx = 1;
+        gbc.gridy = 3;
+        contentPane.add(passwordField, gbc);
 
-        //Ventana de Correo
         JLabel lblCorreo = new JLabel("Correo");
-        lblCorreo.setBounds(130, 225, 61, 16);
-        contentPane.add(lblCorreo);
+        lblCorreo.setForeground(Color.GREEN);
+        lblCorreo.setFont(new Font("Miriam Libre", Font.BOLD, 14));
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        contentPane.add(lblCorreo, gbc);
 
-        //Texto fecha de nacimiento
-        textFieldFechaNacimiento = new JTextField();
-        textFieldFechaNacimiento.setBounds(188, 258, 130, 26);
-        contentPane.add(textFieldFechaNacimiento);
-        textFieldFechaNacimiento.setColumns(10);
-
-        //Ventana de fecha de nacimiento 
-        JLabel lblFechaNacimiento = new JLabel("Fecha de nacimiento");
-        lblFechaNacimiento.setBounds(60, 263, 130, 16);
-        contentPane.add(lblFechaNacimiento);
+        textFieldCorreo = new JTextField();
+        textFieldCorreo.setForeground(Color.BLACK);
+        textFieldCorreo.setColumns(30); // Aumentamos el número de columnas para que el campo sea más largo
+        textFieldCorreo.setFont(new Font("Comic Sans MS", Font.PLAIN, 12));
+        gbc.gridx = 1;
+        gbc.gridy = 4;
+        contentPane.add(textFieldCorreo, gbc);
 
         JButton btnRegistrarse = new JButton("Registrarse");
-        btnRegistrarse.addActionListener(new ActionListener() {
+        btnRegistrarse.setForeground(Color.BLACK);
+        btnRegistrarse.setBackground(Color.WHITE);
+        btnRegistrarse.setFont(new Font("Miriam Libre", Font.BOLD, 18));
+        btnRegistrarse.setPreferredSize(new Dimension(150, 30));
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        contentPane.add(btnRegistrarse, gbc);
+
+        JButton btnIniciarSesión = new JButton("Iniciar Sesión");
+        btnIniciarSesión.setForeground(Color.BLACK);
+        btnIniciarSesión.setBackground(Color.WHITE);
+        btnIniciarSesión.setFont(new Font("Miriam Libre", Font.BOLD, 12));
+        btnIniciarSesión.setPreferredSize(new Dimension(125, 25));
+        GridBagConstraints gbc_btnIniciarSesión = new GridBagConstraints();
+        gbc_btnIniciarSesión.insets = new Insets(10, 10, 10, 10);
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        contentPane.add(btnIniciarSesión, gbc);
+     // ActionListener para el botón "Iniciar Sesión"
+        btnIniciarSesión.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // Aquí puedes agregar la lógica para registrarse
+                // Cierra la ventana de Registro
+                dispose();
+                // Abre la ventana de Inicio De Sesión
+                InicioSesion inicioSesion = new InicioSesion();
+                inicioSesion.setVisible(true);
             }
         });
-        btnRegistrarse.setBounds(157, 296, 117, 29);
-        contentPane.add(btnRegistrarse);
     }
 }
 
