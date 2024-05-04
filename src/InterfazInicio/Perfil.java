323d4ad4f11d39397e6ea3a09992cb3cbc7a34eb
CarlosRMDev <crm0027@alu.medac.es>
2024-05-04 02:03:17 +0200

    comentario

--- src/InterfazInicio/Perfil.java
+++ src/InterfazInicio/Perfil.java
@@ -159,6 +159,23 @@ public class Perfil extends JFrame {
 					lblPuntosMax.setFont(new Font("Arial", Font.PLAIN, 18));
 					lblPuntosMax.setBounds(50, 131, 275, 30);
 					panel.add(lblPuntosMax);
+					
+					//Para poner BRONCE / PLATA / ORO
+					/*
+					 * Podemos hacer un if utilizando el dato que recogemos de la base de datos para introducir
+					 * tres label en la misma posición delante de la puntuación máxima, hacerlos set visible false
+					 * y con los if, else if manejamos cuando aparece uno u otro, y hacemos un ranking de divisiones
+					 * Lo hacemos sólo en el del círculo porque no hay más tiempo
+					 * 
+					 * if (Funciones.SelectContador() <= 95){
+					 * noseque.setVisible(true);
+					 * }
+					 * else if (Funciones.SelectContador() > 95 && Funciones.SelectContador() < 250{
+					 * noseque2.setVisible(true);
+					 * noseque.setVisible(false);
+					 * 
+					 * }
+					 */
 								
 					// Label Número de Partidas
 					JLabel lblNumPartidas = new JLabel("Número de Partidas: " + Funciones.SelectContador());
